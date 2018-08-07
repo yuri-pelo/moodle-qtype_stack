8e695f6de75dfec8df47285768970fba3c743981
Ian Wild <ianwild@luminaconsultancy.com>
2018-08-07 20:59:53 +0100

    Load AMD module

--- renderer.php
+++ renderer.php
@@ -86,8 +86,10 @@ class qtype_stack_renderer extends qtype_renderer {
 
         // Initialise automatic validation, if enabled.
         if ($qaid && stack_utils::get_config()->ajaxvalidation) {
-            $this->page->requires->yui_module('moodle-qtype_stack-input',
-                    'M.qtype_stack.init_inputs', array($inputstovaldiate, $qaid, $qa->get_field_prefix()));
+        /*    $this->page->requires->yui_module('moodle-qtype_stack-input',
+                    'M.qtype_stack.init_inputs', array($inputstovaldiate, $qaid, $qa->get_field_prefix()));*/
+            $this->page->requires->js_call_amd('qtype-stack/input', 'init_inputs', array($inputstovaldiate, $qaid, $qa->get_field_prefix()));
+            
         }
 
         $result = '';
